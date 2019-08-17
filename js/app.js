b0140a1f91a7d8eea7b2daced028262bcb50d734
Diana Kim <dianajkim@gmail.com>
2019-08-17 06:08:54 -0700

    Added 6th question

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var hasPets = prompt('Do you think I have any pets?');
 
 hasPets = hasPets.toLowerCase();
 //Check that input is valid using an if statement
-if(hasPets === 'y' || hasPets === 'yes') {
+if (hasPets === 'y' || hasPets === 'yes') {
   alert('You answered yes. You are correct! I have 4 dogs.');
   //tracks number of correct answers
   numCorrect++;
@@ -22,13 +22,13 @@ if(hasPets === 'y' || hasPets === 'yes') {
 document.getElementById('question-1').innerHTML = 'I have 4 dogs.';
 //Print a useful and descriptive console.log()
 console.log('Answer to "has pets" question: ' + hasPets);
-console.log(numCorrect + ' of 5 correct');
+console.log(numCorrect + ' of 7 correct');
 
 //Second yes/no question
 var hasSiblings = prompt('Do you think I have any siblings?');
 hasSiblings = hasSiblings.toUpperCase();
 
-if(hasSiblings === 'Y' || hasSiblings === 'YES') {
+if (hasSiblings === 'Y' || hasSiblings === 'YES') {
   alert('You answered yes. You are correct! I have one sister and two brothers.');
   numCorrect++;
 } else if (hasSiblings === 'N' || hasSiblings === 'NO') {
@@ -39,13 +39,13 @@ if(hasSiblings === 'Y' || hasSiblings === 'YES') {
 
 document.getElementById('question-2').innerHTML = 'I have one sister and two brothers.';
 console.log('Answer to "has siblings" question: ' + hasSiblings);
-console.log(numCorrect + ' of 5 correct');
+console.log(numCorrect + ' of 7 correct');
 
 //Third yes/no question
 var isTrilingual = prompt('Am I trilingual?');
 isTrilingual = isTrilingual.toUpperCase();
 
-if(isTrilingual === 'Y' || isTrilingual === 'YES') {
+if (isTrilingual === 'Y' || isTrilingual === 'YES') {
   alert('You answered yes. You are incorrect! I speak one language fluently and French and Korean very poorly.');
 } else if (isTrilingual === 'N' || isTrilingual === 'NO') {
   alert('You answered no. Correct!');
@@ -56,13 +56,13 @@ if(isTrilingual === 'Y' || isTrilingual === 'YES') {
 
 document.getElementById('question-3').innerHTML = 'I am fluent in just the English language.';
 console.log('Answer to "is trilingual" question: ' + isTrilingual);
-console.log(numCorrect + ' of 5 correct');
+console.log(numCorrect + ' of 7 correct');
 
 //Fourth yes/no question
 var likesIceClimbing = prompt('Do I like to scale icy mountains?');
 likesIceClimbing = likesIceClimbing.toUpperCase();
 
-if(likesIceClimbing === 'Y' || likesIceClimbing === 'YES') {
+if (likesIceClimbing === 'Y' || likesIceClimbing === 'YES') {
   alert('You answered yes. Wrong! Ice climbing has no appeal for me whatsoever.');
 } else if (likesIceClimbing === 'N' || likesIceClimbing === 'NO') {
   alert('You answered no. Correct!');
@@ -73,13 +73,13 @@ if(likesIceClimbing === 'Y' || likesIceClimbing === 'YES') {
 
 document.getElementById('question-4').innerHTML = 'I\'ve never tried ice climbing and NEVER want to.';
 console.log('Answer to "like ice climbing" question: ' + likesIceClimbing);
-console.log(numCorrect + ' of 5 correct');
+console.log(numCorrect + ' of 7 correct');
 
 //Fifth yes/no question
 var likesCheesecake = prompt('Do I think cheesecake is the best?');
 likesCheesecake = likesCheesecake.toUpperCase();
 
-if(likesCheesecake === 'Y' || likesCheesecake === 'YES') {
+if (likesCheesecake === 'Y' || likesCheesecake === 'YES') {
   alert('You answered yes. You are correct! Cheesecake is my favorite dessert.');
   numCorrect++;
 } else if (likesCheesecake === 'N' || likesCheesecake === 'NO') {
@@ -90,16 +90,51 @@ if(likesCheesecake === 'Y' || likesCheesecake === 'YES') {
 
 document.getElementById('question-5').innerHTML = 'Cheesecake is heavenly.';
 console.log('Answer to "likes cheesecake" question: ' + likesCheesecake);
-console.log(numCorrect + ' of 5 correct');
+console.log(numCorrect + ' of 7 correct');
 
-//Need to refactor above to follow DRY rule
+//Sixth question
+// Prompt user for a Number
+// User gets 4 chances
+// Test if too high or too low
+var myNum = 26;
+var userNum;
+var chances = 4;
+
+for(var i = 0; i <=4; i++) {
+  userNum = parseFloat(prompt(gamer + ', guess how many dog beds I have.'));
+
+  if (userNum > myNum) {
+    alert('Oh darn, that\'s too high.');
+  } else if (userNum < myNum) {
+    alert('Your guess is too low.');
+  } else if (isNaN(userNum)) {
+    alert('That was in invalid guess.');
+  } else if (userNum === myNum) {
+    alert('Yahoo! You got it right, ' + gamer + '. \nIf you have dogs, you\'ll understand why I have so many beds for them!');
+    numCorrect++;
+    document.getElementById('question-6').innerHTML = 'I have 26 dog beds scattered throughout my house and car for my 4 pups.';
+    console.log('Answer to "how many dog beds" question: ' + userNum);
+    console.log(numCorrect + ' of 7 correct');
+    break;
+  }
+  chances--;
+
+  if (chances >= 1) {
+    alert('You have ' + chances + ' more chances.');
+  } else {
+    alert('You\'re all out of chances.');
+    console.log('Answer to "how many dog beds" question: ' + userNum);
+    console.log(numCorrect + ' of 7 correct');
+    break;
+  }
+}
 
 //Display final score
-var yourScore = (numCorrect / 5) * 100;
-if(numCorrect >= 4) {
-  alert('You got '+ yourScore + '% correct. Fantastic job!');
+var yourScore = Math.round((numCorrect / 7) * 100);
+if (numCorrect >= 5) {
+  alert('You got ' + numCorrect + ' out of 7. That\'s '+ yourScore + '% correct. Fantastic job, ' + gamer + '!');
 } else {
-  alert('You got ' + numCorrect + ' out of 5. That\'s only ' + yourScore + '% correct. Better luck next time.');
+  alert('You got ' + numCorrect + ' out of 7. That\'s only ' + yourScore + '% correct. Better luck next time, ' + gamer + '.');
 }
 
 // //Store correct answers in variables for comparison
