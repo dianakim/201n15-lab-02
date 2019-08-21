5e8b3c66d5b2b4a284d19da163c79a5b95aae976
Diana Kim <dianajkim@gmail.com>
2019-08-21 00:35:13 -0700

    Fixed broken nav link, bug in #7, and others

--- js/app.js
+++ js/app.js
@@ -103,31 +103,32 @@ var chances = 4;
 for(var i = 0; i <= 4; i++) {
   userNum = parseFloat(prompt(gamer + ', guess how many dog beds I have.'));
 
-  if (userNum > myNum) {
+  if (userNum === myNum) {
+    alert('Yahoo! You got it right, ' + gamer + '. \nIf you have dogs, you\'ll understand why I have so many beds for them!');
+    numCorrect++;
+    break;
+  } else if (userNum > myNum) {
     alert('Oh darn, that\'s too high.');
   } else if (userNum < myNum) {
     alert('Your guess is too low.');
   } else if (isNaN(userNum)) {
     alert('That was in invalid guess.');
-  } else if (userNum === myNum) {
-    alert('Yahoo! You got it right, ' + gamer + '. \nIf you have dogs, you\'ll understand why I have so many beds for them!');
-    numCorrect++;
-    document.getElementById('question-6').innerHTML = 'I have 26 dog beds scattered throughout my house and car for my 4 pups.';
-    console.log('Answer to "how many dog beds" question: ' + userNum);
-    console.log(numCorrect + ' of 7 correct');
-    break;
-  }
+  } //end if
   chances--;
-
-  if (chances >= 1) {
+  //Alert gamer to number of chances left.
+  //Break out of the for loop if there are no more chances left
+  if (chances > 1) {
     alert('You have ' + chances + ' more chances.');
+  } else if (chances === 1) {
+    alert('You have ' + chances + ' more chance.');
   } else {
     alert('You\'re all out of chances.');
-    console.log('Answer to "how many dog beds" question: ' + userNum);
-    console.log(numCorrect + ' of 7 correct');
     break;
   }
 }
+document.getElementById('question-6').innerHTML = 'I have 26 dog beds scattered throughout my house and car for my 4 pups.';
+console.log('Answer to "how many dog beds" question: ' + userNum);
+console.log(numCorrect + ' of 7 correct');
 
 //Seventh question
 // Prompt user to guess another country I've been in
@@ -137,46 +138,46 @@ for(var i = 0; i <= 4; i++) {
 var myCountries = ['Germany', 'Indonesia', 'Nepal', 'Belize', 'France', 'England', 'Italy', 'The Netherlands', 'Belgium', 'Turks and Caicos', 'Saint Martin', 'Spain', 'Japan'];
 var userGuess;
 var countryString;
-
-//Loop through each chance to guess
-for(var tries = 0; tries <= 6; tries++) {
+var isAMatch = false;
+//Loop through each attempt to guess
+for(var tries = 1; tries <= 6; tries++) {
   userGuess = prompt(gamer + ', guess one other country I have been to.');
   userGuess = userGuess.toLowerCase();
 
   //Loop through each item in array to compare with guess
   for(var a = 0; a < myCountries.length; a++) {
-    //Check if there's a match to one in the array
-    if (userGuess !== myCountries[a].toLowerCase()) {
-      alert('Sorry, no match.');
-    } else if (userGuess === myCountries[a].toLowerCase()) {
-      alert('Yahoo! You got it right, ' + gamer + '. \n!');
-
+    //Check if there's a match to one in the array'
+    if (userGuess === myCountries[a].toLowerCase()) {
+      alert('Yahoo! You got it right, ' + gamer + '!');
+      isAMatch = true;
       numCorrect++;
-
-      //Loop through each item in array to concatenate for message
-      for(var y = 0; y < myCountries.length; y++) {
-        //Concatenate items in the array for printing later
-        if (y < 1) {
-          countryString = myCountries[y];
-        } else if (y >= 1) {
-          countryString = countryString + ', ' + myCountries[y];
-        }
-      }
-      document.getElementById('question-7').innerHTML = 'Other countries I\'ve visited are ' + countryString + '.';
-      console.log('Answer to "other countries" question: ' + userGuess);
-      console.log(numCorrect + ' of 7 correct');
-    }
-  }
-  //Check if any chances are left
-  if (chances >= 1) {
-    alert('You have ' + chances + ' more chances.');
+      break;
+    } //end if
+  } //end for
+  console.log('Answer to "other countries" question: ' + userGuess);
+  console.log(numCorrect + ' of 7 correct');
+  if (!(isAMatch)) {
+    //Tell gamer how many tries are left
+    if (tries <= 5) {
+      alert('Sorry, no match. You have ' + (6 - tries) + ' more chances.');
+    } else if (tries === 6) {
+      alert('You\'re all out of chances.');
+    } //end if
   } else {
-    alert('You\'re all out of chances.');
-    console.log('Answer to "other countries" question: ' + userGuess);
-    console.log(numCorrect + ' of 7 correct');
     break;
-  }
-}
+  } //end if
+
+  //Loop through each item in array to concatenate for message
+  for(var y = 0; y < myCountries.length; y++) {
+    //Concatenate items in the array for displaying later
+    if (y < 1) {
+      countryString = myCountries[y];
+    } else if (y >= 1) {
+      countryString = countryString + ', ' + myCountries[y];
+    } //end if
+  } //end for
+  document.getElementById('question-7').innerHTML = 'Other countries I\'ve visited are ' + countryString + '.';
+} //end for
 
 //Display final score
 var yourScore = Math.round((numCorrect / 7) * 100);
@@ -184,4 +185,4 @@ if (numCorrect >= 5) {
   alert('You got ' + numCorrect + ' out of 7. That\'s '+ yourScore + '% correct. Fantastic job, ' + gamer + '!');
 } else {
   alert('You got ' + numCorrect + ' out of 7. That\'s only ' + yourScore + '% correct. Better luck next time, ' + gamer + '.');
-}
+} //end if
